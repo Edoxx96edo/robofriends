72fa500eecec0d6d18ef1c6b5e1146a20f05b1df
Edoxx96edo <bertiniedoardo@gmail.com>
2025-10-03 17:17:35 +0200

    Hooks added

--- src/Containers/app.jsx
+++ src/Containers/app.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Component } from "react";
+import {useState, useEffect } from "react";
 import CardList from "../Components/cardList";
 import SearchBox from "../Components/searchBox"
 import "./app.css"
@@ -7,47 +7,43 @@ import  Scroll from "../Components/Scroll"
 
 
 
-class App extends Component{
-    constructor(){
-        super()
-        this.state={
-            robots : [],
-            searchField: ""
-        }
-    }
-    componentDidMount(){
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then(response=> response.json())
-            .then(users => 
-                this.setState({robots : users }))
-    }
 
-    setRobots=(event)=>{
-        this.setState({searchField: event.target.value})
+
+function App(){
+    const [searchField, setSearchField] = useState("")
+    const [robots , setRobots] = useState([])
+
+    
+    useEffect(()=>{
+      fetch("https://jsonplaceholder.typicode.com/users")
+           .then(response=> response.json())
+           .then(users => setRobots(users))
+    },[])
+    
+    const onSearchChange= (event)=>{
+        setSearchField(event.target.value)
 
     }
 
 
-    render(){
 
-        const {robots, searchField} = this.state;
 
-        const robotFiltered = robots.filter(robot=>{
+    const robotFiltered = robots.filter(robot=>{
             return robot.name.toLowerCase().includes(searchField)})
 
-        return !robots.length ?
-            <h1>Loading</h1> :
+    return !robots.length ?
+        <h1>Loading</h1> :
            
             (
                 <div className ="tc" >
                     <h1>Robofriends</h1>
-                    <SearchBox searchChange={this.setRobots}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
                         <CardList robots={robotFiltered} />
                     </Scroll>
                 </div>
             )
-    }
+    
 }
 
 
